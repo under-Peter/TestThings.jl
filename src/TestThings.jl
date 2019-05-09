fec7d90ca332d5e1abd8cdbeb3b94a14d65beb90
Andreas Peter <andreas.peter.ch@gmail.com>
2019-05-09 08:34:35 +0200

    Add foobar with (insufficient) tests

--- src/TestThings.jl
+++ src/TestThings.jl
@@ -22,4 +22,40 @@ bar(xs) = \{x \mid x \leq y \quad \forall y \in xs\}
 "
 bar(xs::Vector{<:Integer}) = -foo(-xs)
 
+
+@doc raw"
+    foobar(xs)
+returns the extrema of `xs`
+"
+
+@doc raw"
+    foobar(xs::Vector{<:Integer})
+returns the extrema of a vector of integers
+"
+function foobar(xs::Vector{<:Integer})
+    if length(xs) == 1
+        x = first(xs)
+        return (x,x)
+    end
+    
+    let T = eltype(xs)
+        foldl(((x1,x2),y) -> (ifelse(x1 > y, y, x1), ifelse(x2 > y, x2, y)), xs, init = (typemax(T), typemin(T)))
+    end
+end
+
+@doc raw"
+    foobar(xs::Vector{<:Real})
+returns the extrema of a vector of integers
+"
+function foobar(xs::Vector{<:Real})
+    if length(xs) == 1
+        x = first(xs)
+        return (x,x)
+    end
+    
+    let T = eltype(xs)
+        foldl(((x1,x2),y) -> (ifelse(x1 > y, y, x1), ifelse(x2 > y, x2, y)), xs, init = (typemax(T), typemin(T)))
+    end
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,5 @@ using Test
     @test foo([1,-1,3,4]) == 4
     @test bar([1,-1,3,4]) == -1
     @test TestThings.greet() == nothing
+    @test TestThings.foobar([1]) == (1,1)
 end
